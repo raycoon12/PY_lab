3e863796e50bc6c692df80c7b89bf67445e40684
raycoon12 <s.z.m.francja@gmail.com>
2026-03-03 11:15:38 +0100

    lab1

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-angle = float(input("kąt: "))
-if not (0 >= angle >= 90):
-    print("Kąt poza zakresem")
-velocity = float(input("prędkość pocz: "))
-print(angle)
-print(velocity)
+import math
+from math import sin,radians
+GRAVITY = 9.81
+def get_input():
+    while True:
+        angle = float(input("kąt: "))
+        if not (angle >= 0 and angle <= 90):
+            print("Kąt poza zakresem")
+        else:
+            break
+    velocity = float(input("prędkość pocz: "))
+    return angle, velocity
+
+def calculate_impact(angle, velocity):
+    z = velocity ** 2 * sin(radians(angle) * 2) / GRAVITY
+    return z
+
+def main():
+    angle, velocity = get_input()
+    print(angle)
+    print(velocity)
+    z = calculate_impact(angle, velocity)
+    print(z)
+
+if __name__ == "__main__":
+    main()
+
