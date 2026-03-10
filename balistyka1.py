bf0b6ca6f889277b9a8de70e3c7c0f57464f9332
raycoon12 <s.z.m.francja@gmail.com>
2026-03-10 10:28:13 +0100

    lab

---
+++ balistyka1.py
@@ -0,0 +1,63 @@
+#import math
+from math import sin, radians
+from random import uniform
+
+GRAVITY = 9.81
+SCALE = 0.9
+LENGTH = 80
+IMPACT_RADIUS = 2
+
+def calculate_impact(angle, velocity, offset):
+    return velocity**2 * sin(radians(angle)*2) / GRAVITY + offset
+
+def get_input():
+    while True :
+        angle = float(input("Kąt: "))
+        if angle < 0 or angle > 90:
+            print("Wartość nie jest odpowiednia!")
+        else:
+            break
+
+    velocity = float(input("Prędkość początkowa: "))
+    return angle, velocity
+
+def print_impact(impact, pos1, pos2):
+    scaled_impact = round(impact*SCALE)
+    ground = ["_"]*LENGTH
+    ground[pos1] = "1"
+    ground[pos2] = "2"
+    try:
+        ground[scaled_impact] = "X"
+    except IndexError:
+        print("Poza skalą")
+    [print(symbol, end="") for symbol in ground]
+    print()
+
+def start_game():
+    start1 = uniform(0,LENGTH/2)
+    start2 = uniform(LENGTH/2+1, LENGTH)
+    return round(start1), round(start2)
+
+def check_hit(impact, pos):
+    return abs(impact - pos) <= IMPACT_RADIUS
+
+def main():
+    pos1, pos2 = start_game()
+    shooter = pos1
+    target = pos2
+    while True:
+        print("Gracz: ", 1 if shooter == pos1 else 2)
+        angle, velocity = get_input()
+        if shooter == pos2:
+            angle = 180 - angle
+        z = calculate_impact(angle, velocity, shooter/SCALE)
+
+        print_impact(z,pos1, pos2)
+        if check_hit(z, target/SCALE):
+            print("Gracz", 1 if shooter == pos1 else 2, "wygrywa")
+            break
+        shooter,target = target,shooter
+
+
+if __name__ == '__main__':
+    main()
---
+++ balistyka2.py
@@ -0,0 +1,9 @@
+import curses
+
+
+def main(stdscr):
+    stdscr.erase()
+    stdscr.key()
+
+if __name__ == "__main__":
+    curses.wrapper(main)
